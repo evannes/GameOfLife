fc1f31a478e1225029899e1bbb08febfc4a3ae73
Elise Haram Vannes <s315575@stud.hioa.no>
2017-01-20 13:57:53 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.*;
+
+public class Controller {
+
+    @FXML
+    private Canvas kanvas;
+}
