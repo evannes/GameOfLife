771598f5f34250be7b5cab9d2ad506ee5a965185
s175295 <s175295@stud.hioa.no>
2017-04-05 18:12:42 +0200

    Førsteutkast til getStats virker

--- src/sample/StatisticsWindow.java
+++ src/sample/StatisticsWindow.java
@@ -1,5 +1,7 @@
 package sample;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,19 +10,81 @@ import java.util.List;
 public class StatisticsWindow {
 
     private DynamicBoard clonedBoard;
+    private final static double ALPHA = 0.5d;
+    private final static double BETA = 3.0d;
+    private final static double GAMMA = 0.25d;
 
     public void setClonedBoard(DynamicBoard clonedBoard) {
         this.clonedBoard = clonedBoard;
-        int i = 0;
-        while (i <= 10) {
-            clonedBoard.nextGeneration();
-            i++;
-        }
+        getStatistics(clonedBoard, 30);
     }
 
+    public int countAlive() {
+        int livingCells = 0;
+        for(int i = 0; i < clonedBoard.getWidth(); i++) {
+            for(int j = 0; j < clonedBoard.getHeight(); j++) {
+                if (clonedBoard.getValue(i, j)) {
+                    livingCells ++;
+                }
+            }
+        }
+        return livingCells;
+    }
+
+    public int sumLivingCellCoordinates() {
+        int livingCells = 0;
+        for(int i = 0; i < clonedBoard.getWidth(); i++) {
+            for(int j = 0; j < clonedBoard.getHeight(); j++) {
+                if (clonedBoard.getValue(i, j)) {
+                    livingCells += i;
+                    livingCells += j;
+                }
+            }
+        }
+        return livingCells;
+    }
+
+    public int similarityMeasure(int alive, int change, int living) {
+        double phi = (ALPHA * alive) + (BETA * change) + (GAMMA * living);
+        return (int) phi;
+    }
+
+
     public int[][] getStatistics(Board clonedBoard, int iterations) {
-        int[][] gameStatistics = new int[3][iterations+1];
-        return  gameStatistics;
+        int[][] stats = new int[3][iterations+1];
+        int[] livingCellsHelper = new int[iterations+1];
+
+        stats[0][0] = countAlive();
+        livingCellsHelper[0] = sumLivingCellCoordinates();
+        System.out.println("Gamma ved t(0) = " + sumLivingCellCoordinates());
+        clonedBoard.nextGeneration();
+        System.out.println("Gamma ved t(1) = " + sumLivingCellCoordinates());
+
+        for (int i = 1; i < iterations+1; i++) {
+            // Populate countAlive part of the array
+            stats[0][i] = countAlive();
+
+            // Populate change in living cells
+            stats[1][i-1] = stats[0][i] - stats[0][i-1];
+
+            // Poulate helper table with living cells
+            livingCellsHelper[i] = sumLivingCellCoordinates();
+
+            // Populates initial value of similarity measure
+            stats[2][i-1] = similarityMeasure(stats[0][i-1], stats[1][i-1], livingCellsHelper[i-1]);
+
+            clonedBoard.nextGeneration();
+        }
+
+        // Correcting values for the last iteration
+        stats[1][iterations] = 0;
+        stats[2][iterations] = similarityMeasure(stats[0][iterations], stats[1][iterations], livingCellsHelper[30]);
+
+        for (int j = 0; j < iterations + 1; j++) {
+            System.out.println(j + ": Alive " + stats[0][j] + ", Change: " + stats[1][j] +
+                    ", Initial phi: " + stats[2][j]);
+        }
+        return stats;
     }
 
 
